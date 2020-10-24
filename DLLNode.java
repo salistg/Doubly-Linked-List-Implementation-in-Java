f916a7aeecb4f90e0dd536391a97a9f71fd85801
Salma <62848194+salistg@users.noreply.github.com>
2020-10-24 23:42:54 +0300

    Added comments

--- DLLNode.java
+++ DLLNode.java
@@ -1,13 +1,15 @@
+//Class to represent a node in a doubly linked list
+
 package DataStructures;
 
 public class DLLNode {
-    int data;
-    DLLNode previous;
-    DLLNode next;
+    int data;                  // data that'll fill the node
+    DLLNode previous;         //a reference to the previous node
+    DLLNode next;            //a reference to the next node
 
-    public DLLNode(int data){
-        this.data=data;
-        this.previous=null;
-        this.next=null;
-    }
-}
+    public DLLNode(int data){      //DLLNode class constructor
+        this.data=data;            //initiaizing data
+        this.previous=null;        //intializing previous node reference to NULL
+        this.next=null;            //initializing next node reference to NULL
+    } //constructor end
+} //end class DLLNODE
